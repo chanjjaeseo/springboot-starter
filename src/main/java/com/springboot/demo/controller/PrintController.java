1e44968c47f5092432399baf403e03b944ba9337
quanchengzhi <quanchengzhi@tuandai.com>
2018-12-10 16:13:03 +0800

    fix

--- src/main/java/com/springboot/demo/controller/PrintController.java
+++ src/main/java/com/springboot/demo/controller/PrintController.java
@@ -5,7 +5,7 @@ import com.springboot.demo.dao.StudentDAO;
 import com.springboot.demo.dao.StudentMapper;
 import com.springboot.demo.domain.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,17 +20,17 @@ public class PrintController {
     @Autowired
     private StudentMapper studentMapper;
 
-    @RequestMapping("/print/test")
+    @GetMapping("/print/test")
     String index() {
         return demoProperties.getAll();
     }
 
-    @RequestMapping("/student")
+    @GetMapping("/student")
     String student() {
         return studentDAO.queryStudentById(1).toString();
     }
 
-    @RequestMapping("/student2")
+    @GetMapping("/student2")
     String student2() {
         Student student = studentMapper.queryStudentById(1);
         return student.toString();

3c4970e4072765fbff866a63c4adf03b619027c2
quanchengzhi <quanchengzhi@tuandai.com>
2019-01-11 09:54:11 +0800

    update

--- src/main/java/com/springboot/demo/config/WebMvcConfig.java
+++ src/main/java/com/springboot/demo/config/WebMvcConfig.java
@@ -1,15 +1,21 @@
 package com.springboot.demo.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.springboot.demo.interceptor.DefaultInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Autowired
     private DefaultInterceptor defaultInterceptor;
@@ -24,6 +30,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        /*
+        1.需要先定义一个convert转换消息的对象；
+        2.添加fastjson的配置信息，比如是否要格式化返回的json数据
+        3.在convert中添加配置信息
+        4.将convert添加到converters中
+         */
+        //1.定义一个convert转换消息对象
+         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        // /2.添加fastjson的配置信息，比如：是否要格式化返回json数据
+         FastJsonConfig fastJsonConfig = new FastJsonConfig();
+         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+         fastConverter.setFastJsonConfig(fastJsonConfig);
+         converters.add(fastConverter);
     }
 
 }
